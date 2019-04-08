e8ac317dfa26259cf3f12adc798012892db29c1b
yash1200 <yashjohri1200@gmail.com>
2019-04-09 00:59:36 +0530

    now better

---
+++ lib/model/Note.dart
@@ -0,0 +1,59 @@
+class Note {
+  int _id, _priority;
+  String _title, _desc, _date;
+
+  Note(this._title, this._date, this._priority, [this._desc]);
+
+  Note.withId(this._id, this._title, this._date, this._priority, [this._desc]);
+
+  int get id => _id;
+
+  String get title => _title;
+
+  String get desc => _desc;
+
+  String get date => _date;
+
+  int get priority => _priority;
+
+  set title(String newTitle) {
+    if (newTitle.length <= 255) {
+      this._title = newTitle;
+    }
+  }
+
+  set desc(String newDesc) {
+    if (newDesc.length <= 255) {
+      this._desc = newDesc;
+    }
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  set priority(int newPriority) {
+    if (newPriority == 1 || newPriority == 2) {
+      this._priority = newPriority;
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['desc'] = _desc;
+    map['date'] = _date;
+    map['priority'] = _priority;
+  }
+
+  Note.fromMapObject(Map<String,dynamic> map){
+    this._id=map['id'];
+    this._title=map['title'];
+    this._desc=map['desc'];
+    this._date=map['date'];
+    this._priority=map['priority'];
+  }
+}
